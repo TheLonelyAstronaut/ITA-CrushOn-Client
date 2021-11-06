e0f3ba6ee0c221adf1f8695cbb7e24c727fb3943
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2021-11-07 01:35:36 +0300

    Delete authentication.saga.ts

--- src/features/authentication/domain/sagas/authentication.saga.ts
+++
@@ -1,5 +0,0 @@
-import {AuthenticationService} from "../../data/api/authentication-service.api";
-
-export function* authenticationSaga(action: any, authenticationService: AuthenticationService) {
-	const response = authenticationService.login(action.login, action.password)
-}

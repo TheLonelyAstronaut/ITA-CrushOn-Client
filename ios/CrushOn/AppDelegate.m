684a9cdc6ba55010ebbfc6c86f58abe993cd72bf
trouble <88496511+vikdan4@users.noreply.github.com>
2022-01-20 21:39:40 +0300

    database is setted on ios

--- ios/CrushOn/AppDelegate.m
+++ ios/CrushOn/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 #import "RNBootSplash.h"
 
@@ -29,6 +30,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
